ac6705fad3feb0bc8f3f77adf4f9fa225f428d51
saziddeveloper <saziddeveloper@gmail.com>
2024-08-10 13:29:54 +0600

    upload full setup

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("inputText").addEventListener("keyup", function(event){
+    const inputText = event.target.value;
+    const deleteButton = document.getElementById("button");
+    if(inputText === "I WANT PERMANENT DELETE @username"){
+        deleteButton.removeAttribute("disabled");
+    }
+    else{
+        deleteButton.setAttribute("disabled", true);
+    }
+});
+
+document.getElementById("button").addEventListener("click", function(){
+    // Delay the action by 5 seconds
+    setTimeout(function() {
+        const mainSection = document.getElementById("main-section");
+        mainSection.style.display = "none";
+        const section2 = document.getElementById("section-2");
+        section2.style.display = "flex";
+    }, 2000);
+});
